9f221248adf45344ea36eadc6e0349c1ab04d9e2
Victoria Friedman <victoria.m.friedman@gmail.com>
2015-12-15 19:18:35 +0000

    nitrous-backup at Tue Dec 15 19:18:35 UTC 2015

--- js/planner.js
+++ js/planner.js
@@ -1,1 +1,13 @@
 // Code Your Solution Here
+
+var name = prompt('Please enter your name:');
+
+var age = prompt('Thank you ' + name + '. Please enter your age: ');
+
+var trip_desitination = prompt("Great " + name +  "! Please enter the destination you'd like to visit:");
+
+var number_of_travelers = prompt("How many people will be traveling with you?");
+
+var type_of_trip = prompt("What type of vacation would you like to have (relaxing, rural, city, adventure, etc");
+
+alert(name + ", age " + age + " would like to go to " + trip_desitination + " with " + number_of_travelers + "people. They would like to have a " +  type_of_trip + "vacation." );
