3936d4d0e388b7bcb09b8624f5a14e92fa1da8e5
salome123 <salobustamante@yahoo.com>
2016-04-02 17:28:52 +0000

    Done.

--- js/planner.js
+++ js/planner.js
@@ -1,1 +1,8 @@
 // Code Your Solution Here
+
+var name= prompt("What is your name?");
+var trip_destination= prompt("Where are you headed?");
+var number_of_travelers= prompt("How many people are you traveling with?");
+var type_of_trip= prompt("What type of trip are you looking for?");
+
+alert("Dear, " +name+ " you are headed to " +trip_destination+ " for a" +type_of_trip+ ". how exciting! You and your " +number_of_travelers+ " are going to induldge in a " +type_of_trip+ " . Have fun!!");
